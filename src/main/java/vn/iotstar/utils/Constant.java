a59053ef23133b306a45ec9667a328dfe5cf5c9f
Nguyen Nhat Minh <nguyennhatminh0514@gmail.com>
2025-09-09 14:48:47 +0700

    Delete unnecessary constant

--- src/main/java/vn/iotstar/utils/Constant.java
+++ src/main/java/vn/iotstar/utils/Constant.java
@@ -1,8 +1,6 @@
 package vn.iotstar.utils;
 
 public class Constant {
-    public static final String SESSION_USERNAME = "username";
     public static final String COOKIE_REMEMBER = "username";
     public static final String REGISTER = "/views/account/register.jsp";
-    public static final String DIR = "D:\\Code\\Web\\upload";
 }
